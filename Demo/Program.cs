a56d93b1708f1c820adcad8d29b32cac043534e4
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-10-06 13:56:46 +0300

    03 – HTTP Request / Response Model

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -27,7 +27,7 @@
             //  - Reusable and testable
             //  - Better organization for large systems 
             #endregion
-            #region Part 03 – HTTP Request / Response Model
+            #region Part 03 – HTTP Request / Response Model 
             // HTTP (HyperText Transfer Protocol) defines how client and server communicate.
             //
             // 🔸 Request: Sent by client (browser) -> Server
